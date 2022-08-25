e0fce9a3395c411f53a572f31ce7e1a3fd5ee848
tamarsh <shai.tamar@gmail.com>
2022-08-25 17:44:18 +0100

    Pre-interview Exercise for Developer Candidates The following exercise will assess your ability to deliver well structured and maintainable code. As a TDD shop we will put equal emphasis on evaluating your tests as we will on the code itself. The exercise should only take 20- 40 minutes; don’t spend much longer than this. Please make sure the code is tested and the tests are clearly shown. Please complete the following exercise in SCALA. If this exercise goes well, you may be asked to make further amendments to your code during a pair programming exercise with a member of the team. Please pick the language you think you will be best able to demonstrate your skill and experience. Please use git to version control your exercise. After completing the first step, please clearly tag the commit so that we can evaluate your approach. You may make other commits to further demonstrate your approach. Please compress your answers and submit them via your agent or send us a link to your Github (or similar) account. Do not send us binaries, they won’t get through our firewalls. Your answers will be used as part of our sifting and are likely to be discussed with your interviewer at later stages. Step 1: Shopping cart ● You are building a checkout system for a shop which only sells apples and oranges. ● Apples cost 60p and oranges cost 25p. ● Build a checkout system which takes a list of items scanned at the till and outputs the total cost ● For example: [ Apple, Apple, Orange, Apple ] => £2.05 ● Make reasonable assumptions about the inputs to your solution; for example, many candidates take a list of strings as input Step 2: Simple offers ● The shop decides to introduce two new offers ○ buy one, get one free on Apples ○ 3 for the price of 2 on Oranges ● Update your checkout functions accordingly Complete the steps in order. Don’t read ahead. At each step build the simplest possible solution which meets our requirement. Tag a git commit after each step so that your approach is clear.

---
+++ untitled/src/Main.scala
@@ -0,0 +1,66 @@
+import scala.io.StdIn.readLine
+import scala.util.control.Breaks.break
+
+object Main {
+  var x: Any => Any = _
+
+  def main(args: Array[String]): Unit = {
+
+    var input: String = ""
+    var isAllDone: Boolean = false
+    var allItems: List[String] = List("Apple", "Orange")
+    var billTotal: Double = 0
+    // counter
+    var counter = Map("Apple" -> 0, "Orange" -> 0)
+
+    var totalVAT: Double = 0.00
+    val appleCost: Double = 0.60
+    var orangeCost: Double = 0.25
+    var VAT: Double = 20.0
+    //
+
+    while (true && !isAllDone) {
+      input = readLine("Next Itema? > ")
+
+
+      input.toUpperCase match {
+        case "HELLO" => println("Hi")
+        case "EXIT" => isAllDone = true
+        case "P" => isAllDone = true
+        case "APPLE" => {
+          billTotal = billTotal + appleCost
+          counter += ("Apple" -> (counter("Apple") + 1))
+        }
+        case "A" => {
+          billTotal = billTotal + appleCost
+          counter += ("Apple" -> (counter("Apple") + 1))
+        }
+        case "ORANGE" => {
+          billTotal = billTotal + orangeCost
+          counter += ("Orange" -> (counter("Orange") + 1))
+        }
+        case "O" => {
+          billTotal = billTotal + orangeCost
+          counter += ("Orange" -> (counter("Orange") + 1))
+        }
+        case _ => {
+          println("Unknown feature, please try again")
+          println("Valid options are: Hello, hi, [p]rint bill, [a]pple, [o]range ")
+        }
+      }
+      if (billTotal > 0) {
+        counter.foreach { case (x, y) => if (y > 0) println(y + " " + x + (if (y > 1) "s") ) }
+        println("Sub Total    :" + f"$billTotal%5.2f")
+      }
+    }
+
+    totalVAT = (billTotal * VAT / 100)
+    println("VAT(" + VAT + "%)   :" + f"$totalVAT%5.2f")
+    billTotal = (billTotal + (billTotal * VAT / 100))
+    println("Total        :" + f"$billTotal%5.2f")
+    println("------------------------------------------")
+    println("All down now, thanks for shopping with us!")
+    println("<<->>")
+
+  }
+}
